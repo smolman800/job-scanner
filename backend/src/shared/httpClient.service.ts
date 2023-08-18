5abe86d460a66a870a9329065f9a6b02dabd288b
Vasant Sachdewa <v.sachdewa@gmail.com>
2023-08-18 17:48:21 +0000

    [#8] Refactored httpClient.service

--- backend/src/shared/httpClient.service.ts
+++ backend/src/shared/httpClient.service.ts
@@ -6,32 +6,53 @@ type Config = {
   headers: object;
 };
 
+type Response<T> = {
+  status: number;
+  data: T;
+};
+
 export abstract class HttpClient {
-  abstract get<T>(url: string, config?: Config): Promise<T>;
-  abstract post<T>(url: string, body?: object, config?: any): Promise<T>;
-  abstract patch<T>(url: string, body?: any, config?: any): Promise<T>;
-  abstract delete<T>(url: string, config?: any): Promise<T>;
+  abstract get<T>(url: string, config?: Config): Promise<Response<T>>;
+  abstract post<T>(
+    url: string,
+    body?: object,
+    config?: any,
+  ): Promise<Response<T>>;
+  abstract patch<T>(
+    url: string,
+    body?: any,
+    config?: any,
+  ): Promise<Response<T>>;
+  abstract delete<T>(url: string, config?: any): Promise<Response<T>>;
 }
 
 @Injectable()
 export class AxiosHttpClientService implements HttpClient {
-  async get<T>(url: string, config?: Config): Promise<T> {
+  async get<T>(url: string, config?: Config): Promise<Response<T>> {
     const response = await axios.get<T>(url, config);
-    return response.data;
+    return response;
   }
 
-  async post<T>(url: string, body?: object, config?: Config): Promise<T> {
+  async post<T>(
+    url: string,
+    body?: object,
+    config?: Config,
+  ): Promise<Response<T>> {
     const response = await axios.post<T>(url, body, config);
-    return response.data;
+    return response;
   }
 
-  async patch<T>(url: string, body?: object, config?: Config): Promise<T> {
+  async patch<T>(
+    url: string,
+    body?: object,
+    config?: Config,
+  ): Promise<Response<T>> {
     const response = await axios.patch<T>(url, body, config);
-    return response.data;
+    return response;
   }
 
-  async delete<T>(url: string, config?: Config): Promise<T> {
+  async delete<T>(url: string, config?: Config): Promise<Response<T>> {
     const response = await axios.delete<T>(url, config);
-    return response.data;
+    return response;
   }
 }
