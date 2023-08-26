4f71719540defbd384347d23ecb477ac47512848
Vasant Sachdewa <v.sachdewa@gmail.com>
2023-08-26 12:41:17 +0000

    [#8] Implemented ScraperFactory

--- backend/src/scraper/application/scrapeJob.service.ts
+++ backend/src/scraper/application/scrapeJob.service.ts
@@ -2,6 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { IdGenerator } from 'src/shared/idGenerator.service';
 import { JobPost } from '../entity/jobPost.entity';
 import { JobDetailDTO, JobPostRepository, Scraper } from './interface';
+import { JobsdbScraperService } from '../jobsdb/jobsdb.service';
+import { Vendor, VendorId } from '../entity/vendor.entity';
+
+@Injectable()
+export class ScraperFactory {
+  constructor(private readonly jobsdbScraper: JobsdbScraperService) {}
+
+  create(vendor: Vendor): Scraper {
+    switch (vendor.id) {
+      case VendorId.JOBSDB:
+        return this.jobsdbScraper;
+      default:
+        throw new Error('Invalid vendorId');
+    }
+  }
+}
 
 @Injectable()
 export class ScrapeJobUseCase {
@@ -9,9 +25,11 @@ export class ScrapeJobUseCase {
     private readonly repository: JobPostRepository,
     private readonly idGenerator: IdGenerator,
     private readonly jobPostEntity: typeof JobPost,
+    private readonly scraperFactory: ScraperFactory,
   ) {}
 
-  async execute(scraper: Scraper, vendorId: string) {
+  async execute(vendor: Vendor) {
+    const scraper = this.scraperFactory.create(vendor);
     const jobDetails: JobDetailDTO[] = await scraper.scrapePosts({
       from: null,
       to: null,
@@ -33,7 +51,7 @@ export class ScrapeJobUseCase {
         jobDescription: jobDetail.jobDescription,
         benefit: jobDetail.benefit,
         industry: jobDetail.industry,
-        vendorId,
+        vendorId: vendor.id,
       });
     });
 
