134df13424266c2ae49dd4dfb3bd23764ad0113c
Vasant Sachdewa <v.sachdewa@gmail.com>
2023-09-13 13:33:59 +0000

    [issue_10] Refactored components to be more responsive

--- frontend/src/app/(component)/Loading.tsx
+++ frontend/src/app/(component)/Loading.tsx
@@ -3,7 +3,7 @@ import LinearProgress from '@mui/material/LinearProgress';
 
 export default function Loading() {
   return (
-    <Box>
+    <Box minWidth={'100%'}>
       <LinearProgress color="inherit" />
     </Box>
   );
