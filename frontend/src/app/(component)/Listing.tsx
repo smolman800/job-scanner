f4367f2eac62dc41573161dc0648c5102a10cea9
Vasant Sachdewa <v.sachdewa@gmail.com>
2023-09-13 00:59:50 +0000

    [mui-integration] Refactored Listing component to use MUI components

--- frontend/src/app/(component)/Listing.tsx
+++ frontend/src/app/(component)/Listing.tsx
@@ -1,31 +1,64 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { Listing } from '../interface';
-import styles from './styles/Listing.module.css';
 
 export default function Listing({ item }: { item: Listing }) {
   return (
     <a href={item.pageUrl} target="_blank">
-      <Box className={styles['listing']} key={item.id}>
-        <Box className={styles['listing__logo']}>
-          <img src="/jobsdb-logo.png" />
-        </Box>
-        <Box className={styles['listing__titleCompany']}>
+      <Box
+        key={item.id}
+        sx={{
+          backgroundColor: 'white',
+          transition: 'border 0.3s ease',
+          '&:hover': {
+            border: '3px solid black',
+          },
+        }}
+        marginBottom={1}
+        height={140}
+        borderRadius={2}
+        display={'flex'}
+      >
+        <Box
+          component="img"
+          sx={{
+            height: 140,
+            width: 110,
+          }}
+          alt="logo"
+          src="/jobsdb-logo.png"
+          padding={0.5}
+          borderRadius={2}
+        />
+        <Box
+          height={140}
+          display={'flex'}
+          flexDirection={'column'}
+          padding={1}
+          overflow={'hidden'}
+          width={600}
+        >
           <Box className="jobTitle">
             <u>
               <Typography variant="subtitle1">Job Title</Typography>
             </u>
             <Typography variant="h6">{item.jobTitle}</Typography>
           </Box>
-          <Box className={styles['listing__titleCompany--company']}>
+          <Box marginTop={'auto'}>
             <u>
               <Typography variant="body2">Company</Typography>
             </u>
             <Typography variant="subtitle2">{item.company}</Typography>
           </Box>
         </Box>
-        <Box className={styles['listing__salary']}>
-          <Box className={styles['listing__salary--salary']}>
+        <Box
+          height={140}
+          display={'flex'}
+          flexDirection={'column'}
+          padding={1}
+          width={200}
+        >
+          <Box marginTop={'auto'}>
             <u>
               <Typography variant="body2">Salary</Typography>
             </u>
@@ -34,11 +67,17 @@ export default function Listing({ item }: { item: Listing }) {
             </Typography>
           </Box>
         </Box>
-        <Box className={styles['listing__dateIndustry']}>
-          <Box className={styles['listing__dateIndustry--date']}>
+        <Box
+          height={140}
+          width={200}
+          padding={1}
+          display={'flex'}
+          flexDirection={'column'}
+        >
+          <Box marginLeft={'auto'}>
             <Typography variant="body2">{item.postDate}</Typography>
           </Box>
-          <Box className={styles['listing__dateIndustry--industry']}>
+          <Box marginTop={'auto'} marginLeft={'auto'}>
             <Typography variant="subtitle2">{item.industry}</Typography>
           </Box>
         </Box>
